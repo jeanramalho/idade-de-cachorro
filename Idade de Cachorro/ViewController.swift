0878779275935c895ee00f165c86797b5b841a01
jeanramalho <jeanramalho.dev@gmail.com>
2023-06-12 12:05:09 -0300

    feat(ViewController): Cria lógica de cálculo e finaliza o app

--- Idade de Cachorro/ViewController.swift
+++ Idade de Cachorro/ViewController.swift
@@ -13,9 +13,12 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var legendaResultado: UILabel!
     
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
     @IBAction func descobrirIdade(_ sender: Any) {
         
-        legendaResultado.text = "Texto Alterado"
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade do cachorro em anos humanos é: " + String(idade) + " anos"
         
     }
     
